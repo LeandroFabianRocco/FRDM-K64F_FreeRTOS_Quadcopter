b7a18744b53dcb59ba2043da797579bd3b0f98ca
leandroGHsoft <leandro.f.rocco@gmail.com>
2020-09-12 20:03:48 -0300

    Task for motor controlling created and initialization of FTM0 module

--- source/MotorControl_Tasks.c
+++ source/MotorControl_Tasks.c
@@ -6,3 +6,102 @@
  */
 
 
+#include "MotorControl_Tasks.c"
+
+
+
+/*********************************************************************************************
+ * @brief Set the CnV field for PWM channels
+ *
+ * @param FTM peripheral base address.
+ * @param value received from Quadcopter control
+ * @param PWM channel
+ *
+ * @return void
+ *********************************************************************************************/
+void set_pwm_CnV(FTM_Type *base, int32_t value, uint8_t ch)
+{
+	if (value > CnV_MAX)
+		value = CnV_MAX;
+	if (value < CnV_MIN)
+		value = CnV_MIN;
+	base->CONTROLS[ch].CnV = value;
+	FTM_SetSoftwareTrigger(base, true);
+}
+
+
+/*********************************************************************************************
+ * @brief FTM channels initialization
+ *
+ * @param FTM module pointer
+ *
+ * @return void
+ *********************************************************************************************/
+void FTM0_init(FTM_Type *base)
+{
+	ftm_chnl_pwm_signal_param_t ftmParam[4];
+	ftm_config_t ftmInfo;
+
+	// 400Hz
+	uint8_t duty = 40U;
+	uint16_t period = 400U;
+
+	// 250Hz
+	/*uint8_t duty = 25U;
+	uint16_t period = 250U;*/
+
+	// 50Hz
+	/*uint8_t duty = 5U;
+	uint8_t period = 50U;*/
+
+	ftmParam[0].chnlNumber            = (ftm_chnl_t)PWM_CH0;
+	ftmParam[0].level                 = kFTM_HighTrue;
+	ftmParam[0].dutyCyclePercent      = duty;
+	ftmParam[0].firstEdgeDelayPercent = 0U;
+	ftmParam[0].enableDeadtime        = false;
+
+	ftmParam[1].chnlNumber            = (ftm_chnl_t)PWM_CH1;
+	ftmParam[1].level                 = kFTM_HighTrue;
+	ftmParam[1].dutyCyclePercent      = duty;
+	ftmParam[1].firstEdgeDelayPercent = 0U;
+	ftmParam[1].enableDeadtime        = false;
+
+	ftmParam[2].chnlNumber            = (ftm_chnl_t)PWM_CH2;
+	ftmParam[2].level                 = kFTM_HighTrue;
+	ftmParam[2].dutyCyclePercent      = duty;
+	ftmParam[2].firstEdgeDelayPercent = 0U;
+	ftmParam[2].enableDeadtime        = false;
+
+	ftmParam[3].chnlNumber            = (ftm_chnl_t)PWM_CH3;
+	ftmParam[3].level                 = kFTM_HighTrue;
+	ftmParam[3].dutyCyclePercent      = duty;
+	ftmParam[3].firstEdgeDelayPercent = 0U;
+	ftmParam[3].enableDeadtime        = false;
+
+	FTM_GetDefaultConfig(&ftmInfo);
+	FTM_Init(base, &ftmInfo);
+	FTM_SetupPwm(base, ftmParam, 4U, kFTM_EdgeAlignedPwm, period, FTM_SOURCE_CLOCK);
+	FTM_StartTimer(base, kFTM_FixedClock);
+}
+
+
+
+/*********************************************************************************************
+ * @brief Task to control the four Brushless Motors
+ *
+ * @param
+ *
+ * @return void
+ *********************************************************************************************/
+void MotorControl_task(void *pvParameters)
+{
+
+    for (;;)
+    {
+
+    }
+}
+
+
+
+
--- source/MotorControl_Tasks.h
+++ source/MotorControl_Tasks.h
@@ -10,4 +10,65 @@
 
 
 
+/*************************************************************************
+ * Includes
+ ************************************************************************/
+#include "fsl_ftm.h"
+#include "stdint.h"
+#include "fsl_debug_console.h"
+
+
+/*************************************************************************
+ * Definitions
+ ************************************************************************/
+#define FTM_MODULE FTM0
+#define PWM_CH0  0U
+#define PWM_CH1  1U
+#define PWM_CH2  2U
+#define PWM_CH3  3U
+#define LOOPTIME 4U
+
+#define FTM_SOURCE_CLOCK CLOCK_GetFreq(kCLOCK_McgFixedFreqClk)
+
+
+#define CnV_MAX	782U
+#define CnV_MIN	391U
+
+
+/*********************************************************************************************
+ * @brief Set the CnV field for PWM channels
+ *
+ * @param FTM peripheral base address.
+ * @param value received from Quadcopter control
+ * @param PWM channel
+ *
+ * @return void
+ *********************************************************************************************/
+void set_pwm_CnV(FTM_Type *base, int32_t value, uint8_t ch);
+
+
+
+/*********************************************************************************************
+ * @brief FTM channels initialization
+ *
+ * @param FTM module pointer
+ *
+ * @return void
+ *********************************************************************************************/
+void FTM0_init(FTM_Type *base);
+
+
+/*********************************************************************************************
+ * @brief Task to control the four Brushless Motors
+ *
+ * @param
+ *
+ * @return void
+ *********************************************************************************************/
+void MotorControl_task(void *pvParameters)
+
+
+
 #endif /* MOTORCONTROL_TASKS_H_ */
+
+
