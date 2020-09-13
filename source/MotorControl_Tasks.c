edbbce05e7f4429c3f33a9a2d5a7a4e160b2e726
leandroGHsoft <leandro.f.rocco@gmail.com>
2020-09-13 18:21:25 -0300

    Initialization of FTM0 was changed to main function

--- source/MotorControl_Tasks.c
+++ source/MotorControl_Tasks.c
@@ -105,10 +105,6 @@ float throttle2CnV(uint8_t throttle)
  *********************************************************************************************/
 void ControllingMotors_task(void *pvParameters)
 {
-	// FTM0 module initialization
-	FTM0_init(FTM_MODULE);
-	PRINTF("FTM0 module initialized!!\r\n");
-
 	const TickType_t xDelay500ms = pdMS_TO_TICKS(2000);
 
 	uint8_t counter = 0;
