18396fe7cf58c0f0f06a8b1ce9ecd5fd4e3c45aa
leandroGHsoft <leandro.f.rocco@gmail.com>
2020-09-13 21:11:35 -0300

    Structs passed to other file

--- source/FRDM-K64F_FreeRTOS_Quadcopter.c
+++ source/FRDM-K64F_FreeRTOS_Quadcopter.c
@@ -54,34 +54,7 @@
 #include "MotorControl_Tasks.h"
 
 
-/*********************************************************************
- * Structures and enumerations
- ********************************************************************/
-// Enumeration for data valid
-typedef enum
-{
-	TRUE,
-	FALSE
-} DataValid_t;
 
-// Structure for attitude and joystick data
-typedef struct
-{
-	float ePidPITCH;
-	DataValid_t evPitch;
-
-	float ePidRoll;
-	DataValid_t evRoll;
-
-	float ePidYaw;
-	DataValid_t evYaw;
-
-	uint8_t eJoystick;
-	DataValid_t evJoystick;
-
-	uint8_t eThrottle;
-	DataValid_t evThrottle;
-} Attitude_Joystick_Data_t;
 
 
 
