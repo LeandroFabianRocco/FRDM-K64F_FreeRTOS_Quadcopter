c3739c00ee30504bdde36ae0fc1552c62f23b022
leandroGHsoft <leandro.f.rocco@gmail.com>
2020-09-13 21:11:15 -0300

    File creation

---
+++ source/structs.h
@@ -0,0 +1,42 @@
+/*
+ * structs.h
+ *
+ *  Created on: 13 sep. 2020
+ *      Author: leandro
+ */
+
+#ifndef STRUCTS_H_
+#define STRUCTS_H_
+
+/*********************************************************************
+ * Structures and enumerations
+ ********************************************************************/
+// Enumeration for data valid
+typedef enum
+{
+	TRUE,
+	FALSE
+} DataValid_t;
+
+// Structure for attitude and joystick data
+typedef struct
+{
+	float ePidPITCH;
+	DataValid_t evPitch;
+
+	float ePidRoll;
+	DataValid_t evRoll;
+
+	float ePidYaw;
+	DataValid_t evYaw;
+
+	uint8_t eJoystick;
+	DataValid_t evJoystick;
+
+	uint8_t eThrottle;
+	DataValid_t evThrottle;
+} Attitude_Joystick_Data_t;
+
+
+
+#endif /* STRUCTS_H_ */
